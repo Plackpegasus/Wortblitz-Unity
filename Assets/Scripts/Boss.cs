a32588d96648ab37fc6e6890c7a5c7f9a1ed59c7
Plackpegasus <nicos4001@gmail.com>
2020-01-26 18:34:08 +0100

    little refactoring

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -2,13 +2,13 @@
 using UnityEngine;
 using TMPro;
 
-public class Score : MonoBehaviour, IObserver<int>
+public class Boss : MonoBehaviour, IObserver<int>
 {
-    public int playerScore = 0;
-    public int faultScore = 0;
-    public int scoreToBoss = 200;
-    public Despawn despawn;
-    public TextMeshProUGUI text;
+    public Despawn despawnScript;
+
+    private string text;
+    private string hiddenText;
+    private TextMeshPro tmProGUI;
 
     private IDisposable disposable;
 
@@ -34,20 +34,28 @@ public class Score : MonoBehaviour, IObserver<int>
 
     public void OnNext(int value)
     {
-        playerScore += value;
-        Debug.Log("player gained: " + value + "; current score: " + playerScore);
+        
     }
-
 
     // Start is called before the first frame update
     void Start()
     {
-        Subscribe(despawn);
+        tmProGUI = GetComponentInChildren<TextMeshPro>();
+        Debug.Log(tmProGUI);
+        text = tmProGUI.text;
+
+        foreach (var _ in text)
+        {
+            hiddenText += "X";
+        }
+
+        tmProGUI.text = hiddenText;
+        Subscribe(despawnScript);
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = playerScore.ToString();
+
     }
 }
