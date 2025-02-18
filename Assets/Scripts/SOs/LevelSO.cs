c342c337572edab670f20702a714717a4c9e645e
Plackpegasus <nicos4001@gmail.com>
2025-02-18 22:16:57 +0100

    add scriptable objects

---
+++ Assets/Scripts/SOs/LevelSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelSO", menuName = "Scriptable Objects/LevelSO")]
+public class LevelSO : ScriptableObject
+{
+    public decimal scoreToBoss;
+    public int maxWordLenght;
+}
